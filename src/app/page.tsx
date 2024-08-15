dbc77d201b8890aed5a5c356748fcb961752b972
Asuma Yamada <yamadaasuma@gmail.com>
2024-08-15 12:07:39 -0400

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { WasmTest } from "@/components/WasmTest";
+
+export default function Home() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <WasmTest />
+    </div>
+  );
+}
